d351703dac5bda6afb7addd62d4afd8642eaf18b
Ivan Yakovlev <mail.siiix@yandex.ru>
2023-02-23 14:51:01 +0300

    +++

--- Program.cs
+++ Program.cs
@@ -13,80 +13,20 @@ double p = new Random().NextDouble()*(max-min)+min*/
 
 // double p = new Random().NextDouble()*(max-min)+min
 
-// using System;
-// using static System.Console;
-
-// Clear();
-// PrintArray (GetArray(10,10, 0.2, 5.5));
-
-// double [,] GetArray(int m, int n, double min, double max)
-// {
-//     double [,] result = new double [m, n];
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             result[i, j] = new Random().NextDouble()*(max-min)+min;
-//         }
-
-//     }
-//     return result;
-// }
-
-
-// void PrintArray(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Write($"{array[i, j]} ");
-//         }
-//         WriteLine();
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
- /*Задача 50. Напишите программу, 
-которая на вход принимает позиции элемента в двумерном массиве,
- и возвращает значение этого элемента или же указание, что такого элемента нет.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-[1, 7] -> такого числа в массиве нет*/
-
-
 using System;
 using static System.Console;
 
-
-Write("Введите номер строки ");
-int rows = int.Parse(ReadLine());
-
-Write("Введите номер столбца ");
-int columns = int.Parse(ReadLine());
-
 Clear();
-PrintArray (GetArray(5, 5, 0, 10), rows, columns);
+PrintArray (GetArray(10,10, 0.2, 5.5));
 
-
-int [,] GetArray(int m, int n, int min, int max)
+double [,] GetArray(int m, int n, double min, double max)
 {
-    int [,] result = new int [m, n];
+    double [,] result = new double [m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(min, max+1);
+            result[i, j] = new Random().NextDouble()*(max-min)+min;
         }
 
     }
@@ -94,7 +34,7 @@ int [,] GetArray(int m, int n, int min, int max)
 }
 
 
-void PrintArray(int[,] array, int x, int y)
+void PrintArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -103,14 +43,19 @@ void PrintArray(int[,] array, int x, int y)
             Write($"{array[i, j]} ");
         }
         WriteLine();
-    } 
-    
-    
-    WriteLine();
-    if (x<array.GetLength(0) && y<array.GetLength(1))
-    WriteLine($"{array[x, y]} ");
-    else WriteLine("Такого числа нет");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
       
 
